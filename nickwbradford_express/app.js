93b1eba7f2fc7ff87be22160f7af37ef7582c0f1
bradforn <bradforn@oregonstate.edu>
2018-09-17 07:18:47 -0700

    Switching computers and I think there is some code that was changed but never pushed.

--- nickwbradford_express/app.js
+++ nickwbradford_express/app.js
@@ -17,19 +17,43 @@ app.use(express.static(path.join(__dirname, 'public')));
  * appropriate page title and data for all users to render.
  */
 app.get('/', function (req, res) {
-  res.render('index-page', {
-    title: 'Welcome',
+  res.render('main-page', {
+    title: 'Welcome to My Website',
 
   });
 });
 
 app.get('/about', function (req, res) {
   res.render('about-page', {
-    title: 'About Us',
+    title: 'About Me',
 
   });
 });
 
+app.get('/resume', function (req, res) {
+  res.render('resume-page', {
+    title: 'Resume',
+
+  });
+});
+app.get('/projects', function (req, res) {
+  res.render('projects-page', {
+    title: 'My Projects',
+
+  });
+});
+app.get('/education', function (req, res) {
+  res.render('education-page', {
+    title: 'My Education',
+
+  });
+});
+app.get('/contact', function (req, res) {
+  res.render('contact-page', {
+    title: 'Contact Me',
+
+  });
+});
 
 
 // Return a 404 and render the 404 page for any other route.
